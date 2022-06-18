0ab361273e67166c5fc8220d27206b5ff1880b4e
Moluu <moluu270@gmail.com>
2022-06-18 21:06:08 +0800

    对一些文件进行了修改

--- audio-processing/src/main/java/com/molu/processing/dictionary/MFD.java
+++ audio-processing/src/main/java/com/molu/processing/dictionary/MFD.java
@@ -10,7 +10,7 @@ public class MFD {
     public static final String NCM = ".ncm";
     public static final String MFLAC = ".mflac";
     public static final String FILEPATH = "D:/IDEAproject/molusic/audio-processing/src/main/resources/music/";
-    public static final String UPLOADFILEPATH = "D:/IDEAproject/molusic/audio-processing/src/main/resources/uploadMusic/";
+    public static final String UPLOADFILEPATH = "D:/IDEAproject/molusic/audio-processing/src/main/resources/upload/";
     public static final String LYRICPATH = "D:/IDEAproject/molusic/audio-processing/src/main/resources/lyric/";
     public static final String CODEC = "libmp3lame";
 }
